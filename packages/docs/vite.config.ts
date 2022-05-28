54599c1e28ccfc507edf1ffc67ca7136183bc895
superluckyfir <superluckyfir@qq.com>
2022-05-29 01:14:58 +0800

    chore(docs): set vite base

--- packages/docs/vite.config.ts
+++ packages/docs/vite.config.ts
@@ -1,22 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import shiki from 'markdown-it-shiki'
 import md from 'vite-plugin-md'
 import mdPreview, { transformer } from 'vite-plugin-md-preview'
 
-export default defineConfig({
-  plugins: [
-    vue({
-      include: [/\.vue$/, /\.md$/]
-    }),
-    md({
-      transforms: {
-        before: transformer
-      },
-      markdownItSetup(md) {
-        md.use(shiki, { theme: 'github-light' })
-      }
-    }),
-    mdPreview()
-  ]
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    base: env.NODE_ENV === 'production' ? '/vue-star-plus' : '/',
+    plugins: [
+      vue({
+        include: [/\.vue$/, /\.md$/]
+      }),
+      md({
+        transforms: {
+          before: transformer
+        },
+        markdownItSetup(md) {
+          md.use(shiki, { theme: 'github-light' })
+        }
+      }),
+      mdPreview()
+    ],
+  }
 })
