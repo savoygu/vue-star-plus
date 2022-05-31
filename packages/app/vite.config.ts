05613b7bc3a3786eaf6784397085e21fbd66917d
savoygu <1029559886@qq.com>
2022-05-31 13:20:43 +0800

    chore: use sass-bem-next instead of sass-bem

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "node_modules/sass-bem/_bem.scss";`
+        additionalData: `@import "sass-bem-next/_bem.scss";`
       }
     },
   },
