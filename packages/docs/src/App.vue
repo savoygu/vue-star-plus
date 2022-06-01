89a22e231d8ab757f8bcbf6cd3be70bc71ca05b4
savoygu <1029559886@qq.com>
2022-06-01 10:49:53 +0800

    refactor: docs styles

--- packages/docs/src/App.vue
+++ packages/docs/src/App.vue
@@ -1,36 +1,88 @@
 <script setup lang="ts">
 import VueStarPlusDoc from './markdowns/VueStarPlus.docs.md'
+import pkg from 'vue-star-plus/package.json'
 </script>
 
 <template>
-  <h1>VueStarPlus playground</h1>
+  <header>
+    <h1>VueStarPlus <i>V{{ pkg.version }}</i></h1>
+    <a href="https://github.com/savoygu/vue-star-plus" target="_blank"> Github </a>
+  </header>
   <VueStarPlusDoc></VueStarPlusDoc>
+  <footer>Create by <a href="https://github.com/savoygu" target="_blank">@savoygu</a></footer>
 </template>
 
-<style>
+<style lang="scss">
 #app {
-  margin: 60px auto;
+  width: 1200px;
+  margin: 0 auto;
   color: #1f2f3d;
 }
 
-h1,
-h3 {
-  font-weight: normal;
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 60px;
+
+
+  h1 {
+    position: relative;
+    margin: 0;
+
+    i {
+      position: absolute;
+      top: 0;
+      right: -42px;
+      font-size: 14px;
+      font-style: normal;
+      color: #f05654;
+    }
+  }
 }
 
+footer {
+  height: 40px;
+  line-height: 40px;
+  text-align: center;
+}
+
+header,
+footer {
+  a {
+
+    &:visited,
+    &:hover {
+      color: #0550AE;
+    }
+  }
+}
+
+
+/* common */
 .code-wrapper {
   display: flex;
   flex-wrap: wrap;
   margin-right: 20px;
   color: #ccc;
+
+  .code-block {
+    flex: 1;
+
+    &.code-block-third {
+      width: 33.33%;
+      flex: none;
+    }
+  }
 }
 
-.code-wrapper .code-block {
-  flex: 1;
+/* reset */
+h1,
+h3 {
+  font-weight: normal;
 }
 
-.code-wrapper .code-block.code-block-third {
-  width: 33.33%;
-  flex: none;
+a {
+  text-decoration: none;
 }
 </style>
