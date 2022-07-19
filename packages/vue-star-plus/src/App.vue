5e4d3a6ffc67eb1324aca9bdd6c88b3f42857b76
sfgu <sfgu@hillinsight.com>
2022-07-19 10:36:36 +0800

    refactor(app): change app to vue-star-plus

--- packages/vue-star-plus/src/App.vue
+++ packages/vue-star-plus/src/App.vue
@@ -1,25 +1,23 @@
 <script lang="ts">
-import { computed, defineComponent, ref, watch, ExtractPropTypes } from 'vue'
+import { computed, defineComponent, ref, watch } from 'vue'
 
 const hexColors = /^#([0-9a-fA-F]{6}|[0-9a-fA-F]{3})$/
 const rgbColors = /^[rR][gG][Bb][Aa]?[\(]([\s]*(2[0-4][0-9]|25[0-5]|[01]?[0-9][0-9]?),){2}[\s]*(2[0-4][0-9]|25[0-5]|[01]?[0-9][0-9]?),?[\s]*(0\.\d{1,2}|1|0)?[\)]{1}$/g
 
-function isColor(color: string) {
+const isColor = (color: string) => {
   return hexColors.test(color) || rgbColors.test(color)
-}
-
-const starProps = {
-  modelValue: Boolean,
-  animate: String,
-  color: {
-    type: String,
-    validator: isColor
-  }
 }
 
 export default defineComponent({
   name: 'VueStarPlus',
-  props: starProps,
+  props: {
+    modelValue: Boolean,
+    animate: String,
+    color: {
+      type: String,
+      validator: isColor
+    }
+  },
   emits: ['update:modelValue'],
   setup(props, { emit }) {
     const isActive = ref(props.modelValue)
